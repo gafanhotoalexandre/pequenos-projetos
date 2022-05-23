d844ea349ce33e52f1e3b6261322124a6dd8ae7d
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-05-23 17:30:53 -0300

    criando instancia do axios

--- requests-hooks/src/lib/api.ts
+++ requests-hooks/src/lib/api.ts
@@ -1,10 +1,14 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://jsonplaceholder.typicode.com';
+// const BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+const http = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com'
+});
 
 export const api = {
   async all() {
-    const response = await axios.get(`${BASE_URL}/todos`);
+    const response = await http.get('/todos');
     return response.data;
     /*
     const res = await fetch(`${BASE_URL}/todos`);
@@ -13,7 +17,7 @@ export const api = {
     */
   },
   async create(title: string, body: string, userId: number) {
-    const response = await axios.post(`${BASE_URL}/todos`, {
+    const response = await http.post('/todos', {
       title, body, userId
     });
     return response.data;
