1589b4f1cacad45bade73adf1df572e6171cae3d
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-05-23 17:25:45 -0300

    utilizando axios

--- requests-hooks/src/lib/api.ts
+++ requests-hooks/src/lib/api.ts
@@ -1,12 +1,23 @@
+import axios from 'axios';
+
 const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
 export const api = {
   async all() {
+    const response = await axios.get(`${BASE_URL}/todos`);
+    return response.data;
+    /*
     const res = await fetch(`${BASE_URL}/todos`);
     const data = await res.json();
     return data;
+    */
   },
   async create(title: string, body: string, userId: number) {
+    const response = await axios.post(`${BASE_URL}/todos`, {
+      title, body, userId
+    });
+    return response.data;
+    /*
     const res = await fetch(`${BASE_URL}/todos`, {
       method: 'POST',
       body: JSON.stringify({ title, body, userId }),
@@ -16,5 +27,6 @@ export const api = {
     });
     const data = await res.json();
     return data;
+    */
   },
 };
