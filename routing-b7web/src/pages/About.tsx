b625ca8b3d457b1cf6bbca399ca9cbc1c7c39549
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-05-29 11:41:15 -0300

    utilizando Link

--- routing-b7web/src/pages/About.tsx
+++ routing-b7web/src/pages/About.tsx
@@ -1,7 +1,22 @@
+import { Link } from 'react-router-dom';
+
 export function About() {
   return (
     <div>
-      Sobre
+      <p>Lista de Usuários</p>
+
+      <ul>
+        <li>
+          <Link to="/sobre/bonieky"
+            className="text-blue-600 hover:text-blue-500 transition-all"
+          >Bonieky</Link>
+        </li>
+        <li>
+          <Link to="/sobre/alexandre"
+            className="text-blue-600 hover:text-blue-500 transition-all"
+          >Alexandre</Link>
+        </li>
+      </ul>
     </div>
   );
 }
