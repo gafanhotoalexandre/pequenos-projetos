70549c3b7382afd0dfd31b044a10f23a7f5394c5
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-06-03 20:37:50 -0300

    Navigate component

--- routing/src/App.jsx
+++ routing/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import './App.css';
 
@@ -33,6 +33,7 @@ export default function App() {
 
           <Route path="/search" element={<Search />} />
 
+          <Route path="/company" element={<Navigate to="/about" />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
