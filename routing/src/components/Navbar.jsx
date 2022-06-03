44cac779ac47166be6e30ac88796a0bd51e52693
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-06-02 23:55:02 -0300

    NavLink

--- routing/src/components/Navbar.jsx
+++ routing/src/components/Navbar.jsx
@@ -1,17 +1,15 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 import './Navbar.css';
 
 export function Navbar() {
   return (
     <nav>
-      <Link to="/">
-        Home
-      </Link>
+      {/* <Link to="/">Home</Link> */}
+      {/* <Link to="/about">About</Link> */}
 
-      <Link to="/about">
-        About
-      </Link>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
     </nav>
   );
 }
